ccc2f220f93e301eba4f5308c2765033cb6406ee
Bastian Alessandro Gallegos <bastian4le@gmail.com>
2024-10-27 21:58:53 -0600

    Condicionales

---
+++ ejercicio15.py
@@ -0,0 +1,15 @@
+#Escribe un programa que pida un número entero entre uno y doce e imprima el 
+#número de días que tiene el mes correspondiente.
+# Si introducimos otro número nos da un error.
+
+mes = int(input("Ingresa el mes (1-12): "))
+
+if mes<1 or mes>12:
+    print("Error")
+
+if mes==1 or mes==3 or mes==5 or mes==7 or mes==8 or mes==10 or mes==12:
+    print("El mes tiene 31 días")
+elif mes==4 or mes==6 or mes==9 or mes==11:
+    print("El mes tiene 30 días")
+elif mes==2:
+    print("El mes tiene 28 días")
