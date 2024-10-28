f5b24ad8fa3ce621597def38f6a8bce8e5e9d2b7
Bastian Alessandro Gallegos <bastian4le@gmail.com>
2024-10-27 21:15:08 -0600

    Condicionales

---
+++ ejercicio10.py
@@ -0,0 +1,7 @@
+#Programa que pida tres números y los muestre ordenados (de mayor a menor);
+
+N1 = int(input("Ingresa el primer número: "))
+N2 = int(input("Ingresa el segundo número: "))
+N3 = int(input("Ingresa el tercer número: "))
+
+#GGs
